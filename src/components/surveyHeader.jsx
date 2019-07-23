b2ab0dd8f1f42517f37df4a06ae19373fc8e3f89
Sung <sca@synoint.com>
2019-07-23 14:12:22 +0300

    Survey header component

---
+++ src/components/surveyHeader.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+
+
+const SurveyHeader = () => {
+    return <header className="page-title-box">
+        <div className="page-title-right app-search mt-0">
+            <div className="input-group">
+                <input
+                    type="text"
+                    className="form-control"
+                    placeholder="Search..."
+                />
+                <span className="mdi mdi-magnify" />
+                <div className="input-group-append" />
+                <div className="page-buttons">
+                    <button className="btn btn-primary ml-2">
+                        Preview
+                    </button>
+
+                </div>
+            </div>
+        </div>
+    </header>;
+}
+
+export default SurveyHeader;
